ce70b211c5f493ebb63522f58262f888618f2661
AlI550n <ali550n.q.alg@gmail.com>
2026-05-11 21:36:15 -0400

    Config Logback

--- src/main/java/com/projectmanager/ProjectMannage/applicationservice/ProjectService.java
+++ src/main/java/com/projectmanager/ProjectMannage/applicationservice/ProjectService.java
@@ -6,11 +6,14 @@ import com.projectmanager.ProjectMannage.model.ProjectStatus;
 import com.projectmanager.ProjectMannage.repository.ProjectRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
+
 public class ProjectService {
 
     private final ProjectRepository projectRepository;
@@ -27,7 +30,9 @@ public class ProjectService {
             .build();
 
         projectRepository.save(project);
+        log.info("Project created: {}", project);
         return project;
+
     }
 }
 
